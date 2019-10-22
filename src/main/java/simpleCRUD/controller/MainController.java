13b934c53fbdd4f8df8998a0ebfe4e6aba98d592
TobiasMarsena <tmarsena@gmail.com>
2019-10-22 15:44:31 +0700

    Adding files for Delete feature

---
+++ src/main/java/simpleCRUD/controller/MainController.java
@@ -0,0 +1,22 @@
+package simpleCRUD.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import simpleCRUD.service.StudentServices;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private StudentServices studentServices;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("students", studentServices.getAllStudent());
+		return "index";
+	}
+	
+}
