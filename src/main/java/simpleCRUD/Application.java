b586e4a5e0f7872d9b5a067fbf039d515ee7199d
TobiasMarsena <tmarsena@gmail.com>
2019-10-29 10:41:47 +0700

    Fix javax.persistence.api integration error

--- src/main/java/simpleCRUD/Application.java
+++ src/main/java/simpleCRUD/Application.java
@@ -1,11 +1,13 @@
 package simpleCRUD;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
